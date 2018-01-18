b390e4718e5ec273037b69451781e29dbdd0bd20
Donnyv116 <donnyv116@gmail.com>
2018-01-17 17:55:02 -0700

    homework set up

---
+++ keys.js
@@ -0,0 +1,13 @@
+console.log('this is loaded');
+
+exports.twitter = {
+  consumer_key: XXHiPid6xDCW5ge9rHlT9Pixv,
+  consumer_secret: IdFCyHHGKcwAmKYdzMFjOjl3yvN8oicCmmHtzeayfUmYLxHFS0,
+  access_token_key: 953432687376658432-rJDFXwFiikz4yQ1Fa1HL5TcKjlf61Eh,
+  access_token_secret: nx4AJu2mCocTC6OJySre2CwLB8S8BafHDiU23KAR1eoG5
+};
+
+exports.spotify = {
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+};
